571ef4575ae043e20d97c06555e05eef817dd565
tebit237 <64896943+tebit237@users.noreply.github.com>
2022-11-23 15:20:55 +0100

    routing pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import React from 'react'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import Home from "./pages/Home"
+import Offers from "./pages/Offers"
+import Profile from "./pages/Profile"
+import ForgetPassword from "./pages/ForgetPassword"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 function App() {
   return (
     <Router>
         <Routes>
           <Route path="/" element = {<Home/>}/>
+          <Route path="/profile" element = {<Profile/>}/>
+          <Route path="/sign-in" element = {<SignIn/>}/>
+          <Route path="/sign-up" element = {<SignUp/>}/>
+          <Route path="/forget-password" element = {<ForgetPassword/>}/>
+          <Route path="/offers" element = {<Offers/>}/>
         </Routes>
     </Router>
   );
